376de664c40e0d3da215c2b7d827d25efff3ac54
Ambrose <akindipe.abiola13@gmail.com>
2022-01-11 19:49:14 +0100

    made the required changes

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,55 +1,60 @@
 /**
-* @description A single-page web app that allows users to draw pixel art on a customizable canvas!
-* @constructor
-* @param {string} title - Pixel Art Maker
-* @param {string} author - Akindipe Bukola Abiola (Advanced African scholar)
-*/
+ * @description A single-page web app that allows users to draw pixel art on a customizable canvas!
+ * @constructor
+ * @param {string} title - Pixel Art Maker
+ * @param {string} author - Akindipe Bukola Abiola (Advanced African scholar)
+ */
 
 
 // Select color input
-const colorInput = document.querySelector('#colorPicker').value;
+// const colorInput = document.querySelector('#colorPicker').value;
 
 //Select pixel canvas
 const pixelCanvas = document.querySelector('#pixelCanvas');
 
 // Select size input
-const sizeInput = document.querySelector('#sizePicker');
+// const sizeInput = document.querySelector('#sizePicker');
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-  let gridHeight = document.querySelector('#inputHeight').value;
-  let gridWidth = document.querySelector('#inputWidth').value;
+/**
+ * @desc create a grid of squares 
+ * @param int $width - number of squares representing the width of the grid
+ * @param int $height - number of squares representing the height of the grid
+ */
+function makeGrid(width, height) {
+	let gridHeight = document.querySelector('#inputHeight').value;
+	let gridWidth = document.querySelector('#inputWidth').value;
 
-  // Clear the canvas after every 'submit' event
-  while (pixelCanvas.firstChild) {
-    pixelCanvas.removeChild(pixelCanvas.firstChild);
-    }
+	// Clear the canvas after every 'submit' event
+	while (pixelCanvas.firstChild) {
+		pixelCanvas.removeChild(pixelCanvas.firstChild);
+	}
 
-  // Creating rows and cells in the table
-  for (let i = 1; i <= gridHeight; i++) {
-    let gridRow = document.createElement('tr');
-    pixelCanvas.appendChild(gridRow);
+	// Creating rows and cells in the table
+	for (let i = 1; i <= gridHeight; i++) {
+		let gridRow = document.createElement('tr');
+		pixelCanvas.appendChild(gridRow);
 
-    for (let j = 1; j <= gridWidth; j++) {
-      let gridCell = document.createElement('td');
-      gridRow.appendChild(gridCell);
+		for (let j = 1; j <= gridWidth; j++) {
+			let gridCell = document.createElement('td');
+			gridRow.appendChild(gridCell);
 
-      // Fills cell with selected color when mouse is pressed ('mousedown', unlike 'click', doesn't also require release of mouse button)
-      gridCell.addEventListener('mousedown', function() {
-        const cellColor = document.querySelector('#colorPicker').value;
-        gridCell.style.backgroundColor = cellColor;
-      })
-     }
-  }
+			// Fills cell with selected color when mouse is pressed ('mousedown', unlike 'click', doesn't also require release of mouse button)
+			gridCell.addEventListener('mousedown', function() {
+				const cellColor = document.querySelector('#colorPicker').value;
+				this.style.backgroundColor = cellColor;
+			})
+		}
+	}
 }
 
-makeGrid();
+makeGrid(10, 10);
 
 // using the event method preventDefault() to first intercepting the 'submit' event, which would normally submit the form and refresh the page and preventing makeGrid() from being processed.
 sizePicker.addEventListener('submit', function(event) {
-  event.preventDefault();
-  makeGrid();
+	event.preventDefault();
+	makeGrid();
 });
 
 // Enabling color dragging with the selected color 
@@ -61,26 +66,25 @@ pixelCanvas.addEventListener('mousedown', function(event) {
 	pixelCanvas.addEventListener('mouseup', function() {
 		down = false;
 	});
-  // Prevents cells from coloring if grid is left while pointer is pressed down
-  pixelCanvas.addEventListener('mouseleave', function() {
-    down = false;
-  });
+	// Prevents cells from coloring if grid is left while pointer is pressed down
+	pixelCanvas.addEventListener('mouseleave', function() {
+		down = false;
+	});
 
-  pixelCanvas.addEventListener('mouseover', function(event) {
-    
-    const color = document.querySelector('#colorPicker').value;
-    // While mouse pointer is pressed and within grid boundaries, fills cell with selected color. 
-  	if (down) {
-      // 'TD' capitalized because element.tagName returns upper case for DOM trees that represent HTML elements
-      if (event.target.tagName === 'TD') {
-      	event.target.style.backgroundColor = color;
-      }
-    }
-  });
+	pixelCanvas.addEventListener('mouseover', function(event) {
+
+		const color = document.querySelector('#colorPicker').value;
+		// While mouse pointer is pressed and within grid boundaries, fills cell with selected color. 
+		if (down) {
+			// 'TD' capitalized because element.tagName returns upper case for DOM trees that represent HTML elements
+			if (event.target.tagName === 'TD') {
+				event.target.style.backgroundColor = color;
+			}
+		}
+	});
 });
 
 // Removes color from cell upon double-click
 pixelCanvas.addEventListener('dblclick', function(e) {
-  e.target.style.backgroundColor = null;
+	e.target.style.backgroundColor = null;
 });
-
